46915bd029d587142dc3907b10da03ee6edf153a
Lauren West <laurenwest12@gmail.com>
2018-08-21 22:02:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file.
+let drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function lowerCaseDrivers(driverList) {
+  return driverList.map(driver => driver.toLowerCase())
+}
